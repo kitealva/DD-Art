2057d85235a867450fb5d98c6b09563194ae634b
Brady <kite.alva@gmail.com>
2022-11-25 20:00:18 -0700

    home page

--- server.py
+++ server.py
@@ -54,9 +54,6 @@ def register_user():
 
     return redirect("/")
 
-        
-#####______######_______######_____#######   
-
 
 @app.route("/login", methods=["GET","POST"])
 def process_login():
@@ -74,6 +71,72 @@ def process_login():
         flash(f"You're logged in as {user.email}!")
 
     return redirect("/")
+
+@app.route("/logout", methods=["POST"])
+def process_logout():
+    """Process user logout."""
+
+    logged_in_to_email = session.get("user_email")
+    if logged_in_to_email is None:
+        flash("You were already logged out!")
+    else:
+        del session["user_email"]
+        flash("You're logged out!")
+
+    return redirect("/")
+
+@app.route("/update_quantity", methods=["POST"])
+def update_quantity():
+    quantity_id = request.json["quantity_id"]
+    updated_amount = request.json["updated_amount"]
+    crud.update_quantity(quantity_id, updated_amount)
+    db.session.commit()
+
+    return "Quantity Updated"
+
+@app.route("/cart")
+def cart():
+    """Show art in cart."""
+    
+    
+    get_user = crud.get_user_by_email(session.get("user_email"))
+    
+    cart_art = crud.get_cart_art_by_user_id(get_user.user_id)
+    
+    return render_template("cart.html", cart_art=cart_art)
+
+@app.route("/art/<art_id>/carts", methods=["GET","POST"])
+def create_cart_item(art_id):
+    """Create a new cart item."""
+
+    logged_in_email = session.get("user_email")
+    quantity_amount = request.form.get("quantity")
+
+    if logged_in_email is None:
+        flash("Log in to update quantity!")
+    elif not quantity_amount:
+        flash("Error: you didn't select a quantity.")
+    else:
+        user = crud.get_user_by_email(logged_in_email)
+        art = crud.get_item_by_id(art_id)
+
+        cart_item = crud.create_cart_item(user.user_id, art.art_id, int(quantity_amount))
+        db.session.add(cart_item)
+        db.session.commit()
+
+        flash(f" {art.art_name} have been added to your cart.")
+
+    return redirect(f"/art")
+
+@app.post("/carts/<art_id>/delete")
+def delete_cart(art_id):
+    """Empty cart."""
+    
+    
+    crud.delete_cart_item(art_id)
+    flash(f"Item has been removed.")
+    return redirect("/cart")
+
 
 
 if __name__ == "__main__":
